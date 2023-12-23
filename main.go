a911881653a95fe5936c7685dbd83a9df6f5ad77
Luiz Henrique Silva Sampaio <luizhsampaio07@gmail.com>
2023-12-23 14:27:20 -0300

    :sparkles: feat: Added initial config

--- main.go
+++ main.go
@@ -1,8 +1,19 @@
 package main
 
-import "github.com/LuizSSampaio/gopportunities/internal/router"
+import (
+	"fmt"
+	"github.com/LuizSSampaio/gopportunities/config"
+	"github.com/LuizSSampaio/gopportunities/internal/router"
+)
 
 func main() {
+	// Initialize the config
+	configErr := config.Init()
+	if configErr != nil {
+		fmt.Println(configErr)
+		return
+	}
+
 	// Initialize the router
 	router.Init()
 }
